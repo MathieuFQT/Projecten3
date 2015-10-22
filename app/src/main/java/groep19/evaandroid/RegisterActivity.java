a36f5be06a4373f7986354691201f8a36c0cd575
Nicolas <ngoethals.public@icloud.com>
2015-10-22 13:35:43 +0200

    - Registreren lay-out vervolledigt - Kleurenschema aangepast naar Eva-stijl

---
+++ app/src/main/java/groep19/evaandroid/RegisterActivity.java
@@ -0,0 +1,21 @@
+package groep19.evaandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void registerNewUser(View view){
+        //Register a new user
+        final Button registerBtn = (Button) findViewById(R.id.registerButton);
+        registerBtn.setText("Proficiat!");
+    }
+}
